c9dedeeeb1d3c0b0f85a63915a4bcd58e37158f3
Ankit Gupta <ankitkumargupta752@gmail.com>
2025-07-07 05:08:07 +0530

    Responsive done

--- Frontend/src/services/socket.js
+++ Frontend/src/services/socket.js
@@ -11,7 +11,7 @@ class SocketService {
       return this.socket;
     }
 
-    this.socket = io('http://localhost:5000', {
+    this.socket = io(import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000', {
       transports: ['websocket', 'polling'],
     });
 
